b6d23381653dd29c62ef845654d7584c3bce4bfd
Daniel Volk <mail@volkarts.com>
2026-01-21 20:08:19 +0100

    Fix IPO

--- cmake/Optimizations.cmake
+++ cmake/Optimizations.cmake
@@ -2,10 +2,11 @@ include(CheckIPOSupported)
 
 check_ipo_supported(RESULT IPO_SUPPORTED OUTPUT IPO_CHECK_ERROR)
 
-function(enable_ipo)
+macro(enable_ipo)
     if(NOT IPO_SUPPORTED)
         message(STATUS "IPO / LTO not supported: ${IPO_CHECK_ERROR}")
     else()
+        message(STATUS "IPO / LTO enabled for all builds")
         set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ON)
     endif()
-endfunction()
+endmacro()
