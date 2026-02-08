cbb9fce4f5c7122fdc0c0ef2988243f31aaf3039
Daniel Volk <mail@volkarts.com>
2026-02-08 15:49:13 +0100

    Don't let the container do safety checks

--- src/ngn/utils/StaticVector.hpp
+++ src/ngn/utils/StaticVector.hpp
@@ -8,6 +8,8 @@ namespace ngn {
 template<typename T, std::size_t Capacity, std::unsigned_integral SizeT = uint32_t>
 class StaticVector
 {
+    static_assert(Capacity <= std::numeric_limits<SizeT>::max(), "Size type cannot hold capacity");
+
 public:
     using pointer = T*;
     using const_pointer = const T*;
@@ -37,47 +39,41 @@ public:
 
     reference front()
     {
-        if (size_ == 0)
-            throw std::out_of_range("StaticVector::front() called on empty container");
+        assert(size_);
         return data_[0];
     }
 
     const_reference front() const
     {
-        if (size_ == 0)
-            throw std::out_of_range("StaticVector::front() called on empty container");
+        assert(size_);
         return data_[0];
     }
 
     reference back()
     {
-        if (size_ == 0)
-            throw std::out_of_range("StaticVector::back() called on empty container");
+        assert(size_);
         return data_[size_ - 1];
     }
     const_reference back() const
     {
-        if (size_ == 0)
-            throw std::out_of_range("StaticVector::back() called on empty container");
+        assert(size_);
         return data_[size_ - 1];
     }
 
     reference at(SizeT index)
     {
-        if (index >= size_)
-            throw std::out_of_range("StaticVector index out of bounds");
+        assert(index < size_);
         return data_[index];
     }
 
     const_reference at(SizeT index) const
     {
-        if (index >= size_)
-            throw std::out_of_range("StaticVector index out of bounds");
+        assert(index < size_);
         return data_[index];
     }
 
     template<typename... Args>
-    reference emplace_back(Args... args)
+    reference emplace_back(Args&&... args)
     {
         if (size_ >= Capacity)
             throw std::out_of_range("StaticVector capacity exceeded");
@@ -88,8 +84,7 @@ public:
 
     void pop_back()
     {
-        if (size_ == 0)
-            throw std::out_of_range("StaticVector::pop_back() called on empty container");
+        assert(size_);
         --size_;
         std::destroy_at(data_ + size_);
     }
