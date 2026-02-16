30a0a4bde3ea7909dc2f439362dbfe2a9d04506e
Daniel Volk <mail@volkarts.com>
2026-02-16 20:18:37 +0100

    Renumber layers - the right way

--- src/maze/Layers.hpp
+++ src/maze/Layers.hpp
@@ -5,13 +5,13 @@
 
 #include "phys/Layers.hpp"
 
-constexpr auto LayerWalls = ngn::Layers::L1;
-constexpr auto LayerBoundaries = ngn::Layers::B1;
+constexpr auto LayerBoundaries = ngn::Layers::B0;
+constexpr auto LayerWalls = ngn::Layers::L0;
 
+constexpr auto LayerOpponents = ngn::Layers::B1;
 constexpr auto LayerPlayer = ngn::Layers::L4;
 constexpr auto LayerEnemies = ngn::Layers::L5;
-constexpr auto LayerOpponents = ngn::Layers::B2;
 
+constexpr auto LayerShots = ngn::Layers::B2;
 constexpr auto LayerPlayerShots = ngn::Layers::L8;
 constexpr auto LayerEnemyShots = ngn::Layers::L9;
-constexpr auto LayerShots = ngn::Layers::B3;
